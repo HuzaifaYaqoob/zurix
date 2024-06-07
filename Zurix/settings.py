581b2efc75af194fad2df69a84e7a3cab1c6b216
HuzaifaYaqoob <zeevhyorg@gmail.com>
2024-06-07 11:21:35 +0500

    logo added

--- Zurix/settings.py
+++ Zurix/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-^4bd=zk432hvw779ac-jgxc$mbtj2vl))bsy305^j&zfqg#e(-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
